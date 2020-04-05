c7e9bbdf226c46fa9ffad6a571c6b126a670e70c
RemiLassalle <63157021+RemiLassalle@users.noreply.github.com>
2020-04-05 21:52:03 +0200

    Add files via upload

---
+++ SpaceInvaders.java
@@ -0,0 +1,73 @@
+package fr.unilim.iut.spaceinvaders;
+
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
+
+public class SpaceInvaders {
+	 private static final char MARQUE_FIN_LIGNE = '\n';
+	 private static final char MARQUE_VAISSEAU = 'V';
+	 int longueur;
+	 int hauteur;
+	 Vaisseau vaisseau;
+
+	    public SpaceInvaders(int longueur, int hauteur) {
+		   this.longueur = longueur;
+		   this.hauteur = hauteur;
+	   }
+	    
+	    public void positionnerUnNouveauVaisseau(int x, int y) {
+			
+			if (  !estDansEspaceJeu(x, y) )
+				throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+		
+			vaisseau = new Vaisseau(x, y); 
+	    }
+	    
+	    public void deplacerVaisseauVersLaDroite() {
+	        if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
+		}
+	    
+	    public void deplacerVaisseauVersLaGauche() {
+	    	 if (vaisseau.abscisse()< (longueur-1)&&(vaisseau.abscisse()>0)) vaisseau.seDeplacerVersLaGauche();
+		}
+
+		public boolean estDansEspaceJeu(int x, int y) {
+			return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
+		}
+	    
+	   
+	    
+	    @Override
+		public String toString() {
+			return recupererEspaceJeuDansChaineASCII();
+		}
+
+		public String recupererEspaceJeuDansChaineASCII() {
+			StringBuilder espaceDeJeu = new StringBuilder();
+			for (int y = 0; y < hauteur; y++) {
+				for (int x = 0; x < longueur; x++) {
+					espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
+				}
+				espaceDeJeu.append(MARQUE_FIN_LIGNE);
+			}
+			return espaceDeJeu.toString();
+		}
+
+		private char recupererMarqueDeLaPosition(int x, int y) {
+			char marque;
+			if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+			      marque=MARQUE_VAISSEAU;
+			else
+			      marque='.';
+			return marque;
+		}
+
+		private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+			return this.aUnvaisseau() && vaisseau.occupeLaPosition(x, y);
+		}
+
+		private boolean aUnvaisseau() {
+			return vaisseau!=null;
+		}
+
+}
